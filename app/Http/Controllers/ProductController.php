29ebe51491114402708d3bfc1b18c79be632e073
MuradMagomedov <magomedoff84@gmail.com>
2022-12-25 21:29:23 +0300

    #lara/php/doc add: установил всевозможные зависимости, добавил комменты в контроллер

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,42 +8,48 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-
-
+    //Получаем товары на главной + сортировка
     public function Index(Request $request)
     {
         return view('mytemp.pages.products', ['data' => $this->getAll($request)]);
     }
+
+    //Получаем товары на странице платья + сортировка
     public function Platya(Request $request)
     {
         $categoryId = '1';
         return view('mytemp.pages.products', ['data' => $this->getCategory($request, $categoryId)]);
     }
+
+    //Получаем товары на странице куртки + сортировка
     public function Kurtki(Request $request)
     {
         $categoryId = '2';
         return view('mytemp.pages.products', ['data' => $this->getCategory($request, $categoryId)]);
     }
 
+    //Получаем товары на странице Джемперы + сортировка
     public function Jempers(Request $request)
     {
         $categoryId = '3';
         return view('mytemp.pages.products', ['data' => $this->getCategory($request, $categoryId)]);
     }
 
-
+    //Получаем товары на странице Костюмы + сортировка
     public function Costums(Request $request)
     {
         $categoryId = '4';
         return view('mytemp.pages.products', ['data' => $this->getCategory($request, $categoryId)]);
     }
+
+    //Получаем товары на странице Кардиганы + сортировка
     public function Kardigans(Request $request)
     {
         $categoryId = '5';
         return view('mytemp.pages.products', ['data' => $this->getCategory($request, $categoryId)]);
     }
 
-
+    //Сортировка по категориям
     private function getCategory($request, string $categoryId)
     {
         $products = Product::where('category_id', '=', $categoryId);
@@ -65,6 +71,8 @@ class ProductController extends Controller
         }
         return $products->get();
     }
+
+    // сортировка на главной
     private function getAll($request)
     {
         $products = Product::all();
