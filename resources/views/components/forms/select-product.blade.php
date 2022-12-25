0e994926710a8103b950182af25b18a58e0d9bb8
MuradMagomedov <magomedoff84@gmail.com>
2022-12-25 21:21:59 +0300

    #html add: мелкие правки

--- resources/views/components/forms/select-product.blade.php
+++ resources/views/components/forms/select-product.blade.php
@@ -1,8 +1,8 @@
 <div>
 <form action="" method="get">
     <select name='select' class="select_category">
-        <option value='abc'>по abc </option>
-        <option value='cba'>по cba </option>
+        <option value='abc'>а до я </option>
+        <option value='cba'>я до а </option>
         <option value='priceMax'>сначала дорогие</option>
         <option value='priceMin'>сначала дешевые</option>
     </select>
